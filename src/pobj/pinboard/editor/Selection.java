c71f08c04b531dc5f31b22b5aaeaac8fcf643cc9
NessimBou <36245071+NessimBou@users.noreply.github.com>
2018-03-29 15:03:07 +0200

    salut

---
+++ src/pobj/pinboard/editor/Selection.java
@@ -0,0 +1,41 @@
+package pobj.pinboard.editor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.scene.canvas.GraphicsContext;
+import pobj.pinboard.document.Board;
+import pobj.pinboard.document.Clip;
+
+public class Selection {
+
+	private List<Clip> list = new ArrayList<>();
+
+	
+	public void select(Board board,double x, double y){
+		clear();
+		for(Clip e : board.getContents()){
+			if(e.isSelected(x, y)){
+				list.add(e);
+				break;
+			}
+		}
+	}
+	
+	public void toogleSelect(Board board, double x,double y){
+		
+	}
+	
+	public void clear(){
+		list.clear();
+	}
+	
+	public List<Clip> getContents(){
+		return list;
+	}
+	
+	public void drawFeedBack(GraphicsContext gc){
+		
+	}
+	
+}
